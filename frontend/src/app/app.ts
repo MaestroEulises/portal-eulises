63c5a8c23481a7d67c2a46b9e68c739085268268
tahlia bernal <thalia.bernal.mt@corro.usa.edu.co>
2026-09-22 21:12:09 -0500

    Ajuste boton whatsapp header e inclusion de pagina bienvenida

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { Footer } from './components/footer/footer';
 import { Header } from './components/header/header';
 import { Starfield } from './components/starfield/starfield';
+import { WelcomeModal } from './components/welcome-modal/welcome-modal';
 import { WhatsAppButton } from './components/whatsapp-button/whatsapp-button';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Footer, WhatsAppButton, Starfield],
+  imports: [RouterOutlet, Header, Footer, WhatsAppButton, Starfield, WelcomeModal],
   templateUrl: './app.html',
 })
 export class App {}
